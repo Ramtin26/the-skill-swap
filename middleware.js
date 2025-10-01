0367cc2462e8f83b93656000b300cfc029484085
Ramtin26 <uprivate9@gmail.com>
2025-10-01 14:25:56 +0330

    testing

--- middleware.js
+++ middleware.js
@@ -29,3 +29,13 @@
 // export const config = {
 //   matcher: ["/dashboard/:path*", "/onboarding/:path*"],
 // };
+
+import { NextResponse } from "next/server";
+
+export function middleware() {
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/onboarding/:path*"],
+};
